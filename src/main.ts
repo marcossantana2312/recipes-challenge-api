a446cf5bb14c7ab7f840937d6be16486b81487d4
Marcos da Silva Santana <marcos.santana@cubos.io>
2020-12-01 20:02:07 -0300

    feat: add env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { config } from "dotenv";
 import { AppModule } from "./app.module";
 
+config();
+
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
+  
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(PORT, () => {
+      if(!process.env.RECIPIES_URL || !process.env.GIPHY_KEY) {
+        console.warn(`Some of the following variables:
+          RECIPIES_URL (${process.env.RECIPIES_URL}), GIPHY_KEY (${process.env.GIPHY_KEY}) was not set`);
+      }
+      console.log(`listening at http://localhost:${PORT}`);
+    });
 }
 bootstrap();
